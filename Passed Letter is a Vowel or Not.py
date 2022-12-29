205cdc9cc95c0df391420436f3674862cf74cc84
Rasik Khandla <121342457+Rasik01@users.noreply.github.com>
2022-12-29 20:59:02 +0530

    Update Passed Letter is a Vowel or Not.py

--- Passed Letter is a Vowel or Not.py
+++ Passed Letter is a Vowel or Not.py
@@ -1,7 +1,7 @@
 #Write a Python program to test whether a
 #passed letter is a vowel or not
 L=input("Enter Letter: ")
-if L in "aeiou" or "AEIOU":
+if L in "aeiouAEIOU":
     print(L," letter is Vowel")
 else:
     print(L," letter is not Vowel")
